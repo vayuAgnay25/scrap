4e34732d7cd6b4b84b904e87a22898be558ad644
vayu <Vayuswaraj2212521@gmail.com>
2025-10-23 11:21:22 +0530

    commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,"../frontend")));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,`../frontend/index.html`));
+})
+
+module.exports = app;
