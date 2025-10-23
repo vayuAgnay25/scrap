699d2918666ec446d3894996b5e0765dd1d7a2bc
vayu <Vayuswaraj2212521@gmail.com>
2025-10-23 12:26:07 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
-const express = require('express');
+const express = require("express");
+const server = express();
 const path = require('path');
-const app = express();
 
-app.use(express.static(path.join(__dirname,"../frontend")));
+server.use(express.static(path.join(__dirname,"../../frontend")));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,`../frontend/index.html`));
-})
+const links = [
+  "/",
+  "/dashboard",
+  "/entries",
+  "/gallery",
+  "/contacts",
+  "/passwords",
+  "/login",
+  "/home",
+  "/signup",
+];
 
-module.exports = app;
+function renderPage(res, link) {
+  res.sendFile(path.join(__dirname, `../${link.toLowerCase()}.html`));
+}
+
+links.forEach((link) => {
+  server.get(`${link.toLowerCase()}`, (req, res) => {
+    renderPage(res, "index");
+  });
+});
+
+module.exports = server;
