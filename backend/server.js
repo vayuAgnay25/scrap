15b5d2f68ec835fc609d012accf0d5410cca3139
vayu <Vayuswaraj2212521@gmail.com>
2025-10-23 11:29:43 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,"../frontend")));
 
-app.get('/',(req,res)=>{
+app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname,`../frontend/index.html`));
 })
 
